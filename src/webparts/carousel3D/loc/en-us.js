f86a221bcf36e50a5339eb09e336420428b91398
olivier.carpentier@gmail.com <ocarpen@microsoft.com>
2016-10-02 10:13:06 +0200

    Init commit

---
+++ src/webparts/carousel3D/loc/en-us.js
@@ -0,0 +1,122 @@
+define([], function() {
+  return {
+    "PropertyPaneDescription": "Customize your 3D carousel",
+    "BasicGroupName": "Play settings",
+    "DescriptionFieldLabel": "Description Field",
+    "SpeedFieldLabel": "Speed",
+    "AutoplayFieldLabel": "Autoplay",
+    "AutoplayDelayFieldLabel": "Autoplay delay",
+    "BringtoFrontFieldLabel": "Bring to front",
+    "ShowButtonsFieldLabel": "Show buttons",
+    "MirrorGapFieldLabel": "Mirror Gap",
+    "MirrorHeightFieldLabel": "Mirror height",
+    "MirrorOpacityFieldLabel": "Mirror opacity",
+    "YOriginFieldLabel": "Y Origin",
+    "YRadiusFieldLabel": "Y Radius",
+    "XOriginFieldLabel": "X Origin",
+    "XRadiusFieldLabel": "X Radius",
+    "MirrorGroupName": "Mirror",
+    "GeneralGroupName": "General",
+    "OriginGroupName": "Origin & Radius",
+    "ShowTitleFieldLabel": "Show title",
+    "DataGroupName": "Data",
+    "DataFieldLabel": "Carousel items",
+    "TitleGroupName": "Title",
+    "FontFieldLabel": "Font",
+    "FontSizeFieldLabel": "Size",
+    "ColorFieldLabel": "Color",
+    "HeightFieldLabel": "Height",
+    "ItemHeightFieldLabel": "Item Height",
+    "ReadMore": "Read more",
+
+    //SPListQuery labels
+    //Copy the following labels in your project if you want to use the SPListQuery
+    "SPListQueryList": "List",
+    "SPListQueryOrderBy": "Order By",
+    "SPListQueryArranged": "Arranged",
+    "SPListQueryMax": "Max",
+    "SPListQueryAdd": "Add filter",
+    "SPListQueryRemove": "Remove",
+    "SPListQueryOperatorEq": "Equals",
+    "SPListQueryOperatorNe": "Not Equals",
+    "SPListQueryOperatorStartsWith": "Starts With",
+    "SPListQueryOperatorSubstringof": "Sub String of",
+    "SPListQueryOperatorLt":  "Less than",
+    "SPListQueryOperatorLe": "Less or equal to",
+    "SPListQueryOperatorGt": "Greater than",
+    "SPListQueryOperatorGe": "Greater or equal to",
+
+    //PicturePicker labels
+    //Copy the following labels in your project if you want to use the PicturePicker
+    "PicturePickerTitle": "Select a picture",
+    "PicturePickerRecent": "Recent",
+    "PicturePickerSite": "Site",
+    "PicturePickerButtonSelect": "Select a picture",
+    "PicturePickerButtonReset": "Reset",
+
+    //DocumentPicker labels
+    //Copy the following labels in your project if you want to use the DocumentPicker
+    "DocumentPickerTitle": "Select a document",
+    "DocumentPickerRecent": "Recent",
+    "DocumentPickerSite": "Site",
+    "DocumentPickerButtonSelect": "Select a document",
+    "DocumentPickerButtonReset": "Reset",
+
+    //PeoplePicker labels
+    //Copy the following labels in your project if you want to use the PeoplePicker
+    "PeoplePickerSuggestedContacts": "Suggested contacts",
+    "PeoplePickerNoResults": "No result found",
+    "PeoplePickerLoading": "Loading contacts...",
+
+    //SPListPicker labels
+    //Copy the following labels in your project if you want to use the SPListPicker
+    "SPListPickerLoading": "Loading lists...",
+
+    //SPFolderPicker labels
+    //Copy the following labels in your project if you want to use the SPFolderPicker
+    "SPFolderPickerDialogTitle": "Select a folder",
+    "SPFolderPickerSelectButton": "Select",
+    "SPFolderPickerCancelButton": "Cancel",
+
+    //DatePicker labels
+    //Copy the following labels in your project if you want to use the DatePicker
+    "DatePickerMonthLongJanuary": "January",
+    "DatePickerMonthShortJanuary": "Jan",
+    "DatePickerMonthLongFebruary": "February",
+    "DatePickerMonthShortFebruary": "Feb",
+    "DatePickerMonthLongMarch": "March",
+    "DatePickerMonthShortMarch": "Mar",
+    "DatePickerMonthLongApril": "April",
+    "DatePickerMonthShortApril": "Apr",
+    "DatePickerMonthLongMay": "May",
+    "DatePickerMonthShortMay": "May",
+    "DatePickerMonthLongJune": "June",
+    "DatePickerMonthShortJune": "Jun",
+    "DatePickerMonthLongJuly": "July",
+    "DatePickerMonthShortJuly": "Jul",
+    "DatePickerMonthLongAugust": "August",
+    "DatePickerMonthShortAugust": "Aug",
+    "DatePickerMonthLongSeptember": "September",
+    "DatePickerMonthShortSeptember": "Sept",
+    "DatePickerMonthLongOctober": "October",
+    "DatePickerMonthShortOctober": "Oct",
+    "DatePickerMonthLongNovember": "November",
+    "DatePickerMonthShortNovember": "Nov",
+    "DatePickerMonthLongDecember": "December",
+    "DatePickerMonthShortDecember": "Dec",
+    "DatePickerDayLongSunday": "Sunday",
+    "DatePickerDayShortSunday": "Sun",
+    "DatePickerDayLongMonday": "Monday",
+    "DatePickerDayShortMonday": "Mon",
+    "DatePickerDayLongTuesday": "Tuesday",
+    "DatePickerDayShortTuesday": "Tue",
+    "DatePickerDayLongWednesday": "Wednesday",
+    "DatePickerDayShortWednesday": "Web",
+    "DatePickerDayLongThursday": "Thursday",
+    "DatePickerDayShortThursday": "Thu",
+    "DatePickerDayLongFriday": "Friday",
+    "DatePickerDayShortFriday": "Fri",
+    "DatePickerDayLongSaturday": "Saturday",
+    "DatePickerDayShortSaturday": "Sat"
+  }
+});
